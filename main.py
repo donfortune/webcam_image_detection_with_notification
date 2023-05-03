45467d8605c93b12f9b1171d410fd78993b9cf28
donfortune <donfortunet.df@gmail.com>
2023-05-03 11:15:08 +0100

    converted bgr to grayscale and added blur

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ video = cv2.VideoCapture(0) #opens your webcam
 time.sleep(1)
 while True:
 
-    check, frame = video.read()
-    cv2.imshow('My Video', frame)
+    check, frame = video.read() #two variables because the video.read method returns two values
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert rgb frame to gray scale frame
+    gray_blur = cv2.GaussianBlur(gray_frame, (21, 21), 0) #blur the grayscale image
+
+    cv2.imshow('My Video', gray_blur)
 
 
     key = cv2.waitKey(1) #create keyboard key object
