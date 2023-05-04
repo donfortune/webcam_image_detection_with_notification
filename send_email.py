7e1549cc0cc504c7d3266df96ba6912657fbcbf8
donfortune <donfortunet.df@gmail.com>
2023-05-04 13:32:34 +0100

    finished email function with attachment

--- send_email.py
+++ send_email.py
@@ -1,2 +1,28 @@
-def send_email():
-    print('Email was sent')
+
+import smtplib
+import ssl
+import imghdr  #gives metadata about images
+from email.message import EmailMessage
+def send_email(image_path):
+    host = "smtp.gmail.com"
+    port = 465
+    username = 'donfortunet.df@gmail.com'
+    password = 'kktoyewnkmctutke'
+    receiver_email = 'osowoayiobi@gmail.com'
+    message = EmailMessage()
+    message['Subject'] = 'New Image Found'
+    message.set_content('New image')
+
+    with open(image_path, 'rb') as file:
+        content = file.read()
+        message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as file:
+        file.login(username, password)
+        file.sendmail(username, receiver_email, message.as_string())
+        file.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path='images/19.jpeg')
